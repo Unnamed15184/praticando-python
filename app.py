c3cd73c4dbe645c257f11723a5d9203e1b8bbd92
Ad Silva <adriannounnamed@gmail.com>
2024-11-05 11:46:42 -0200

    testando função

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print('Testando o primeiro projeto!')
+print('Testando uma função')
+
+def calcular_preco_combo(pizza, refrigerante):
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 5)
+calcular_preco_combo(50, 8)
+calcular_preco_combo(100, 10)
+print('Programa finalizado')
