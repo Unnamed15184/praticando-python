25bdbc63c8c5d41c876920295b06cfce2921609a
Ad Silva <adriannounnamed@gmail.com>
2024-11-05 12:07:21 -0200

    testando input e print com os dados digitados

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ calcular_preco_combo(30, 5)
 calcular_preco_combo(50, 8)
 calcular_preco_combo(100, 10)
 print('Programa finalizado')
+
+name = (input('Digite seu nome: '))
+email = (input('Digite seu email'))
+
+print(f'Olá {name}, seja bem vindo!')
+print(f'Seu email é o {email} ?')
